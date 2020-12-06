44109d77a661b7343c411f348f9d8adc9bd3da56
jaume <jaumepies@gmail.com>
2020-12-06 10:40:25 +0100

    -Eliminada carpeta Exams. -Duplicada el package Common per a que es pugui obrir Server i Client en finestres diferents.

---
+++ OnlineMultipleChoiceExamServer/src/common/Exam.java
@@ -0,0 +1,53 @@
+package common;
+
+
+import java.util.ArrayList;
+
+public class Exam {
+    private ArrayList<Quiz> quizzes;
+    private boolean isFinished;
+    private String result = "0.0";
+
+    public Exam(ArrayList<Quiz> quizzes) {
+        this.quizzes = quizzes;
+        this.isFinished = false;
+    }
+
+    public Quiz getNextQuiz(){
+        for(Quiz q: quizzes){
+            if(q.getSelectedChoice() == null){
+                return q;
+            }
+        }
+        return null;
+    }
+
+    public void setQuiz( Quiz quiz){
+        quizzes.set(quiz.getId(), quiz);
+    }
+
+    public String calculateResult() {
+        int correct = 0;
+
+        // For each quiz check if the correct answer matches with selected choice
+        for(Quiz q: quizzes){
+            if(q.getSelectedChoice() != null && q.getSelectedChoice() == q.getCorrectAnswer()){
+                correct++;
+            }
+        }
+
+        result = String.valueOf(((float)correct / quizzes.size()) * 10.0);
+
+        return result;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+
+    public String getResult() { return result; }
+}
---
+++ OnlineMultipleChoiceExamServer/src/common/OMCEClient.java
@@ -0,0 +1,30 @@
+package common;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface OMCEClient extends Remote {
+    void notifyStartExam() throws RemoteException;
+
+    void notifyRegisterStudent() throws RemoteException;
+
+    boolean isRegistered() throws RemoteException;
+
+    String getStudentId() throws RemoteException;
+
+    void notifyQuiz(String quiz) throws RemoteException;
+
+    String inputAnswer() throws RemoteException;
+
+    void notifyResult(String result) throws RemoteException;
+
+    void notifyRegisteredStudent() throws RemoteException;
+
+    boolean isCorrectId(String id) throws RemoteException;
+
+    void leaveSession() throws RemoteException;
+
+    void setAnswer(String answer) throws RemoteException;
+
+    String getAnswer() throws RemoteException;
+}
